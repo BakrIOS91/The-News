fc74b9155dcf8068961ccbb9c0ee95143c98c1e9
Bakr Mohamed <Engbakrmohamed@hotmail.com>
2022-11-14 13:56:31 +0200

    Initial Commit

---
+++ TheNews/TheNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheNewsApp.swift
+//  TheNews
+//
+//  Created by Bakr mohamed on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
