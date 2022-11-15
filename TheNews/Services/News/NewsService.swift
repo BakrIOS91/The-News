13e5aa265a98dba51f2461e23ce8c6199c4b377c
Bakr Mohamed <Engbakrmohamed@hotmail.com>
2022-11-15 13:15:09 +0200

    add News Service

--- TheNews/Services/News/NewsService.swift
+++ TheNews/Services/News/NewsService.swift
@@ -5,4 +5,41 @@
 //  Created by Bakr mohamed on 15/11/2022.
 //
 
+import ComposableArchitecture
+import XCTestDynamicOverlay
 import Foundation
+
+
+struct NewsClient {
+    var list: (_ query: String,_ pageIndexx: Int) async throws -> NewsList
+}
+
+extension DependencyValues {
+  var newsClient: NewsClient {
+    get { self[NewsClient.self] }
+    set { self[NewsClient.self] = newValue }
+  }
+}
+
+extension NewsClient: TestDependencyKey {
+    static var previewValue: NewsClient {
+        return .init(
+            list: { _, _ in .mock }
+        )
+    }
+    
+    static var testValue: NewsClient {
+        return .init (
+        list: unimplemented("\(Self.self).List")
+        )
+    }
+}
+
+extension NewsClient: DependencyKey {
+    static let liveValue = NewsClient (
+        list: { query, pageIndex in
+            let request = NewsRequests.getNewsList(query: query, pageIndex: pageIndex, pageSize: kAppPageSize)
+            return try await APIFetcher.shared.fetch(request: request, responseClass: NewsList.self)
+        }
+    )
+}
--- TheNews/Utilities/AppConstants.swift
+++ TheNews/Utilities/AppConstants.swift
@@ -7,3 +7,4 @@ import Foundation
 let kAppLanguage = "kAppLanguage"
 let kAppRootView = "kAppRootView"
 let kAppleLanguage = "AppleLanguages"
+let kAppPageSize: Int = 10
